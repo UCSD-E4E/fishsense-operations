7b57b6307a80e2823f47abc81e9097f6dcaad8b3
Nathan Hui <nthui@ucsd.edu>
2025-03-01 16:13:08 -0800

    feat: Adds argparse

--- dive_discovery.py
+++ dive_discovery.py
@@ -1,45 +1,40 @@
-# %%
 from pathlib import Path
-
+import argparse
 import pandas as pd
 from tqdm.auto import tqdm
 
 from backend import get_dive_checksum, get_dive_date
 
-# %%
-data_root = Path('//e4e-nas.ucsd.edu/fishsense_data/REEF/data')
+def process(data_root: Path):
 
-# %%
-df = pd.DataFrame()
+    df = pd.DataFrame()
 
-# %%
-images = data_root.rglob('*.ORF')
+    images = data_root.rglob('*.ORF')
 
-# %%
-dives = {img.parent.absolute() for img in images}
+    dives = {img.parent.absolute() for img in images}
 
-# %%
-df['directory'] = [dive.as_posix() for dive in dives]
+    df['directory'] = [dive.as_posix() for dive in dives]
 
-# %%
-df['date'] = None
-df['invalid_image'] = None
-df['multiple_date'] = None
-df['checksum'] = None
+    df['date'] = None
+    df['invalid_image'] = None
+    df['multiple_date'] = None
+    df['checksum'] = None
 
-# %%
-for idx, row in tqdm(df.iterrows(), total=df.shape[0]):
-    path = Path(row['directory'])
-    mean_date, invalid_dates, multiple_dates = get_dive_date(path)
-    dive_checksum = get_dive_checksum(path)
-    df.at[idx, 'date'] = mean_date
-    df.at[idx, 'invalid_image'] = invalid_dates
-    df.at[idx, 'multiple_date'] = multiple_dates
+    for idx, row in tqdm(df.iterrows(), total=df.shape[0]):
+        path = Path(row['directory'])
+        mean_date, invalid_dates, multiple_dates = get_dive_date(path)
+        dive_checksum = get_dive_checksum(path)
+        df.at[idx, 'date'] = mean_date
+        df.at[idx, 'invalid_image'] = invalid_dates
+        df.at[idx, 'multiple_date'] = multiple_dates
 
-# %%
-df.to_csv('dives.csv')
+    df.to_csv('dives.csv')
 
-# %%
-
-
-
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_root', type=Path)
+    args = parser.parse_args()
+    process(args.data_root)
+    
+if __name__ == '__main__':
+    main()
