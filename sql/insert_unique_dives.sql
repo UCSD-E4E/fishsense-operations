3affad20afcaac6a748e9bfc267f6a44c6e9bd33
Nathan Hui <nthui@ucsd.edu>
2025-03-01 21:29:01 -0800

    fix: Fixes unique constraint

--- sql/insert_unique_dives.sql
+++ sql/insert_unique_dives.sql
@@ -1,2 +1,3 @@
 INSERT INTO canonical_dives (path, date, invalid_image, multiple_date, checksum)
-SELECT min(path), date, invalid_image, multiple_date, checksum FROM dives GROUP BY checksum;
+SELECT min(path), date, invalid_image, multiple_date, checksum FROM dives GROUP BY checksum
+ON CONFLICT DO NOTHING;
